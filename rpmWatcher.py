644a30543afea134eb9ab54cc0582314f4e198f4
traczpasruchu <tracz.pasruchu@gmail.com>
2023-01-21 23:13:06 -0500

    Initial commit

---
+++ rpmWatcher.py
@@ -0,0 +1,20 @@
+import obd
+import time
+import os
+
+os.system("\"C:\\Program Files (x86)\\Bluetooth Command Line Tools\\bin\\btcom.exe\" -b8C:DE:00:02:4E:EA -c")
+connection = obd.Async("COM3")    # Open asynchronous OBD-II connection
+
+
+# This function will run as a callback every time a new value is found
+def new_rpm(r):
+    print(r.value)
+
+
+connection.watch(obd.commands.RPM, callback=new_rpm)    # Subscribe RPM command to updates with a callback
+connection.start()  # Start the update loop
+
+time.sleep(15)      # Let the udpate loop run for 1 minute
+connection.stop()   # Stop the update loop
+
+os.system("\"C:\\Program Files (x86)\\Bluetooth Command Line Tools\\bin\\btcom.exe\" -b8C:DE:00:02:4E:EA -r")
